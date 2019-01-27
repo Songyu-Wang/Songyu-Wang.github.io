90cc74e4763f87d15f7bdd43aa638be3b29a79cd
Songyu-Wang <wangsongyuf@gmail.com>
2019-01-27 03:32:11 -0800

    fix security issue with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -32,19 +32,7 @@ function getConfig() {
         new webpack.LoaderOptionsPlugin({
             minimize: true
         }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                unused: true,
-                dead_code: true,
-                warnings: true,
-                screw_ie8: true
-            },
-            compressor: {
-                warnings: false
-            },
-            minimize: true,
-            sourceMap: true
-        })
+        
         ]
     };
 }
