c57fc5ccbff990897b685f32ef795b7ad4e9c2d3
Songyu-Wang <wangsongyuf@gmail.com>
2019-01-28 00:05:59 -0800

    add icon to avoid 404

--- app/components/section/footer.js
+++ app/components/section/footer.js
@@ -37,7 +37,7 @@ const Footer = React.createClass({
             </ul>
             <ul>
             <div>
-            Header image from&nbsp;
+            Header and favicon.ico image from&nbsp;
             <a style={{ backgroundColor:'black',color:'white',textDecoration:'none',padding:'4px 6px',fontFamily:'-apple-system, BlinkMacSystemFont, "San Francisco", "Helvetica Neue", Helvetica, Ubuntu, Roboto, Noto, "Segoe UI", Arial, sans-serif',fontSize:'12px',fontWeight:'bold',lineHeight:'1.2',display:'inline-block',borderRadius:'3px' }} href='https://unsplash.com/@iriser?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge' target='_blank' rel='noopener noreferrer' title='Download free do whatever you want high-resolution photos from Irina Iriser'>
             <span style={{ display:'inline-block',padding:'2px 3px' }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox={ '0 0 32 32' } style={{ height:'12px',width:'auto',position:'relative',verticalAlign:'middle',top:'-2px',fill:'white'}}>
